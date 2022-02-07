86fa02bec3674e155eedb560de272297aa19277d
Nikita <saffer1234@mail.ru>
2022-02-07 19:18:39 +0300

    made solve via switch/case statement

--- StaticFinal/02_enum/src/ru/skillbox/ArithmeticCalculator.java
+++ StaticFinal/02_enum/src/ru/skillbox/ArithmeticCalculator.java
@@ -2,32 +2,57 @@ package ru.skillbox;
 
 public class ArithmeticCalculator {
 
-    private int firstNumber;
-    private int secondNumber;
+    private static int firstNumber = 0;
+    private static int secondNumber = 0;
 
     public ArithmeticCalculator(int firstNumber, int secondNumber) {
         this.firstNumber = firstNumber;
         this.secondNumber = secondNumber;
     }
 
-    public int makeCalculation(Enum operation) {
-        if (Operation.ADD.equals(operation)) {
-            return firstNumber + secondNumber;
-        }
-        else if (Operation.SUBTRACT.equals(operation)) {
-            return firstNumber - secondNumber;
-        }
-        else if (Operation.MULTIPLY.equals(operation)) {
-            return firstNumber * secondNumber;
-        }
-        else if (Operation.DIVIDE.equals(operation)) {
-            if (firstNumber !=0 && secondNumber != 0) {
-                return firstNumber / secondNumber;
+    public static int makeCalculation(Operation operation) {
+
+        switch (operation) {
+            case ADD:
+                return firstNumber + secondNumber;
+
+            case SUBTRACT:
+                return firstNumber - secondNumber;
+
+            case MULTIPLY:
+                return firstNumber * secondNumber;
+
+            case DIVIDE : {
+                if (secondNumber != 0) {
+                    return firstNumber / secondNumber;
+                }
+                else {
+                    System.out.println("Нельзя делить на ноль!");
+                }
+                break;
             }
-            else {
-                System.out.println("Одноиз значений равно нулю!");
-            }
+            default:
+                return 0;
+//         if (Operation.ADD.equals(operation)) {
+//            return firstNumber + secondNumber;
+//
+//        }
+//        else if (Operation.SUBTRACT.equals(operation)) {
+//            return firstNumber - secondNumber;
+//        }
+//        else if (Operation.MULTIPLY.equals(operation)) {
+//            return firstNumber * secondNumber;
+//        }
+//        else if (Operation.DIVIDE.equals(operation)) {
+//            if (secondNumber != 0) {
+//                return firstNumber / secondNumber;
+//            }
+//            else {
+//                System.out.println("Нельзя делить на ноль!");
+//            }
+
         }
         return 0;
     }
+
 }
--- StaticFinal/02_enum/src/ru/skillbox/Main.java
+++ StaticFinal/02_enum/src/ru/skillbox/Main.java
@@ -3,7 +3,7 @@ package ru.skillbox;
 public class Main {
 
     public static void main(String[] args) {
-        ArithmeticCalculator arithmeticCalculator = new ArithmeticCalculator(15,5);
+        ArithmeticCalculator arithmeticCalculator = new ArithmeticCalculator(15,0);
 
         System.out.println("сложение: " + arithmeticCalculator.makeCalculation(Operation.ADD));
         System.out.println("вычитание: " + arithmeticCalculator.makeCalculation(Operation.SUBTRACT));

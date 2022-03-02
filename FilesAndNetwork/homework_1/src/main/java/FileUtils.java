0fcb3aad90ae4ec865605a0d3021670ad7e77c99
Nikita <saffer1234@mail.ru>
2022-03-02 17:51:22 +0300

    done homework

--- FilesAndNetwork/homework_1/src/main/java/FileUtils.java
+++ FilesAndNetwork/homework_1/src/main/java/FileUtils.java
@@ -1,6 +1,21 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class FileUtils {
 
     public static long calculateFolderSize(String path) {
-        return 0;
+        long size = 0;
+        try{
+            Path folder = Paths.get(path);
+            size = Files.walk(folder)
+                    .filter(p -> p.toFile().isFile())
+                    .mapToLong(p -> p.toFile().length())
+                    .sum();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        return size;
     }
 }
--- FilesAndNetwork/homework_1/src/main/java/Main.java
+++ FilesAndNetwork/homework_1/src/main/java/Main.java
@@ -1,5 +1,26 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] sizes = new String[] {"B", "KB", "MB", "GB", "TB"};
+        String readablePrint = "";
+        try {
+            String path = scanner.nextLine();
+            long size = FileUtils.calculateFolderSize(path);
+            for (int i = 0; i < sizes.length; i++) {
+                if (size / Math.pow(1024, i+1) < 1 || i == sizes.length -1) {
+                    readablePrint += ((double) Math.round((double) size * 10/ Math.pow(1024,i))) / 10
+                            + " " + sizes[i];
+                    break;
+                }
+            }
+            System.out.println(readablePrint);
+
+        } catch (Exception ex) {
+            ex.getStackTrace();
+        }
 
     }
 }
